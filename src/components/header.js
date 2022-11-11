f4f5a5e3c0dfdd3d1eae29bf4043066e57280bf1
Daron Wiafe <daronkwiafe@gmail.com>
2022-11-11 14:28:23 +0200

    Style application

--- src/components/header.js
+++ src/components/header.js
@@ -2,22 +2,24 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-const Header = () => (
-  <header className="header">
-    <div className='nav-holder'>
-      <p className="logo"><strong>Bookstore CMS</strong></p>
-      <nav>
-        <ul className="nav-link">
-          <li className="link-item" id="books" data-testid="books"><Link to="/">Books</Link></li>
-          <li className="link-item"><Link to="/categories">Categories</Link></li>
-        </ul>
-      </nav>
-    </div>
-    <div className='user-details'>
-      <i className="fa-solid fa-user person"></i>
-    </div>
-    <Outlet />
-  </header>
-);
+const Header = () => {
+  return (
+    <header className="header">
+      <div className='nav-holder'>
+        <p className="logo"><strong>Bookstore CMS</strong></p>
+        <nav>
+          <ul className="nav-link">
+            <li className="link-item" id="books" data-testid="books"><Link to="/">Books</Link></li>
+            <li className="link-item"><Link to="/categories">Categories</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <div className='user-details'>
+        <i className="fa-solid fa-user person"></i>
+      </div>
+      <Outlet />
+    </header>
+  )
+};
 
 export default Header;
